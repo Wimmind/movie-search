8681219a97b8d60777e7401501a0c30a3e44990c
Wimmind <terehov.anton@bk.ru>
2020-05-03 00:12:21 +0400

    feat: add spinner loading

--- movie-search/src/index.js
+++ movie-search/src/index.js
@@ -32,61 +32,66 @@ const swiper = new Swiper('.swiper-container', {
   },
 });
 
-
 const buttonSearch = document.querySelector('.search-button');
-const swiperWrapper = document.querySelector('.swiper-wrapper');
 const buttonClear = document.querySelector('.clear-button');
 
-const clearSwiper = (swiperSlides) => {
-  while (swiperSlides.firstChild) {
-    swiperSlides.removeChild(swiperSlides.firstChild);
-  }
-};
+
 async function getRating(imdbiD) {
-  const url = `https://www.omdbapi.com/?i=${imdbiD}&apikey=49ee8599`;
+  const url = `https://www.omdbapi.com/?i=${imdbiD}&apikey=bea3a778`;
   const res = await fetch(url);
   const data = await res.json();
   return data.imdbRating;
 }
 
-async function getMovieTitle(word) {
+const spinner = document.querySelector('#spinner');
+
+
+
+let toSlide = async (film)=> {
+  const card = document.createElement('div');
+  card.classList.add('card');
+  card.classList.add('swiper-slide');
+
+  const cardLink = document.createElement('a');
+  cardLink.href = `https://www.imdb.com/title/${film.imdbID}/videogallery`;
+  cardLink.target = '_blank';
+  cardLink.innerText = film.Title;
+  cardLink.classList.add('card-header');
+
+  const cardImage = document.createElement('img');
+  cardImage.classList.add('card-body');
+  cardImage.alt = 'poster';
+  cardImage.src = film.Poster === 'N/A' ? '../assets/image/no-poster.jpg' : film.Poster;
+  const cardFooter = document.createElement('div');
+  cardFooter.classList.add('card-footer');
+  cardFooter.innerText = film.Year;
+
+  const cardRating = document.createElement('p');
+  cardRating.innerText = await getRating(film.imdbID);
+  cardFooter.append(cardRating);
+
+  card.append(cardLink);
+  card.append(cardImage);
+  card.append(cardFooter);
+  return card;
+}
+
+async function getMovieSlides(word) {
+
+  spinner.style.display = 'block';
+
   document.querySelector('.info').innerText = '';
-  const url = `https://www.omdbapi.com/?s=${word}&apikey=49ee8599`;
+  const url = `https://www.omdbapi.com/?s=${word}&apikey=bea3a778`;
   const res = await fetch(url);
   const data = await res.json(); // объект фильмов
   if (data.Response === 'True') {
-    clearSwiper(swiperWrapper);
-    data.Search.forEach(async (item) => {
-      const card = document.createElement('div');
-      card.classList.add('card');
-      card.classList.add('swiper-slide');
-
-      const cardLink = document.createElement('a');
-      cardLink.href = `https://www.imdb.com/title/${item.imdbID}/videogallery`;
-      cardLink.target = '_blank';
-      cardLink.innerText = item.Title;
-      cardLink.classList.add('card-header');
-
-      const cardImage = document.createElement('img');
-      cardImage.classList.add('card-body');
-      cardImage.alt = 'poster';
-      cardImage.src = item.Poster === 'N/A' ? '../assets/image/no-poster.jpg' : item.Poster;
-      const cardFooter = document.createElement('div');
-      cardFooter.classList.add('card-footer');
-      cardFooter.innerText = item.Year;
-
-      const cardRating = document.createElement('p');
-      cardRating.innerText = await getRating(item.imdbID);
-      cardFooter.append(cardRating);
-
-      card.append(cardLink);
-      card.append(cardImage);
-      card.append(cardFooter);
-
-      swiper.appendSlide(card);
-    // swiper.update();
-    });
+    const card = data.Search.map((item)=> toSlide(item));
+    
+    await Promise.all(card);
+    //swiper.removeAllSlides();
+    card.forEach(item => item.then(result => swiper.appendSlide(result)))
     document.querySelector('.info').innerText = `Showing results for "${word}"`;
+    swiper.slideTo(0);
   } else {
     if (data.Error === 'Movie not found!') {
       document.querySelector('.info').innerText = `No results for "${word}"`;
@@ -98,16 +103,18 @@ async function getMovieTitle(word) {
       document.querySelector('.info').innerText = 'You did not enter a search query';
     }
   }
+  spinner.style.display = 'none';
 }
-getMovieTitle('rock');
+
+getMovieSlides('love');
+//49ee8599
 
 // проверка языка
 async function languageСheck(word) {
   const url = `https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20200502T075924Z.16bef609e3b7f7e4.bb4d2ed3596546da694c76a845f50ad6c2ef19e1&text=${word}&lang=ru-en`;
   const res = await fetch(url);
   const data = await res.json();
-  getMovieTitle(data.text.join());
-  document.querySelector('.info').innerText = `Showing results for "${data.text.join()}"`;
+  getMovieSlides(data.text.join());
 }
 
 // на кнопку сеарч
@@ -117,22 +124,21 @@ buttonSearch.addEventListener('click', () => {
     if ((word[0].charCodeAt() > 1040) && (word[0].charCodeAt() < 1103)) {
       languageСheck(word);
     } else {
-      getMovieTitle(word);
+      getMovieSlides(word);
     }
   }
 });
 
 // на ентер
 document.addEventListener('keydown', (event) => {
-  const buttonKeyboard = event.key;
-  if (buttonKeyboard === 'Enter') {
+  if (event.key === 'Enter') {
     event.preventDefault();
     const word = document.querySelector('.search-input').value;
     if (word.length>0){
       if ((word[0].charCodeAt() > 1040) && (word[0].charCodeAt() < 1103)) {
         languageСheck(word);
       } else {
-        getMovieTitle(word);
+        getMovieSlides(word);
       }
     }
   }
