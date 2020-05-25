6ffa7fc41295a18721462edc8bce3686626c4a97
Wimmind <terehov.anton@bk.ru>
2020-05-25 17:05:11 +0400

    fix: hasRusLetters func

--- movie-search/src/js/hasRusLetters.js
+++ movie-search/src/js/hasRusLetters.js
@@ -1,8 +1,5 @@
 import {cyrillicReg} from './variables';
 
 export default function hasRusLetters(word) {
-    if (cyrillicReg.test(word)) {
-      return true;
-    }
-    return false; 
+  return cyrillicReg.test(word);
 }
